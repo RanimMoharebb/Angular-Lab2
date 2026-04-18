28766f33f7ffe4f527a76f1df7335c2699ea8d1b
Ranim Mohareb <ranimmohareb650@gmail.com>
2026-04-18 23:29:24 +0200

    Initial commit - Student Management System

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { StudentTableComponent } from './components/student-table/student-table.component';
+import { AddStudentComponent } from './pages/add-student/add-student.component';
+import { StudentDetailsComponent } from './pages/student-details/student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StudentsComponent,
+    StudentTableComponent,
+    AddStudentComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
